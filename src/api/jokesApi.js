2b4a65b1224d74b2d71f868c39833e8d070fe4f2
Juggernau9 <xbhavyatomar@gmail.com>
2020-09-16 23:23:22 +0530

    fetch jokes from api

---
+++ src/api/jokesApi.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "https://sv443.net/jokeapi/v2/joke",
+});
--- src/components/JokeList.js
+++ src/components/JokeList.js
@@ -1,9 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, useContext, useEffect } from "react";
+
+import { CategoryContext } from "../contexts/CategoryContext";
+import { BlacklistContext } from "../contexts/BlacklistContext";
+import jokesApi from "../api/jokesApi";
 
 function JokeList() {
   const [loading, setLoading] = useState(true);
+  const [jokes, setJokes] = useState({});
+  const { flags } = useContext(BlacklistContext);
+  const { categories } = useContext(CategoryContext);
 
-  console.log(loading);
+  useEffect(() => {
+    const getJokes = async () => {
+      let categoryString = "";
+      let flagString = "";
+
+      if (categories.all) {
+        categoryString = "any";
+      } else {
+        for (const key in categories) {
+          if (categories[key]) {
+            categoryString += key.toString() + ",";
+          }
+        }
+        categoryString = categoryString.slice(0, -1);
+      }
+
+      for (const key in flags) {
+        if (flags[key]) {
+          flagString += key.toString() + ",";
+        }
+      }
+      flagString = flagString.slice(0, -1);
+
+      try {
+        const response = await jokesApi.get(`/${categoryString}?amount=6`, {
+          params: {
+            blacklistFlags: flagString || undefined,
+            // contains: search || undefined,
+          },
+        });
+        console.log(response);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getJokes();
+  }, [flags, categories]);
   return <div>{loading ? "loading..." : "Jokes"}</div>;
 }
 
