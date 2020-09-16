9bb20295d9b9b6043d73e509084fd38db5193c08
Juggernau9 <xbhavyatomar@gmail.com>
2020-09-16 15:41:45 +0530

    add drawer from material-ui

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Drawer from "./Drawer";
+
+function App() {
+  return (
+    <div className="App">
+      <Drawer />
+    </div>
+  );
+}
+
+export default App;
