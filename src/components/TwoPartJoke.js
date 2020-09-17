0c447bacb25821ac1acbe568ff203c111ca4402b
Juggernau9 <xbhavyatomar@gmail.com>
2020-09-17 17:19:58 +0530

    add favorite and share icons to card

--- src/components/TwoPartJoke.js
+++ src/components/TwoPartJoke.js
@@ -3,8 +3,11 @@ import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
+// import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import ShareIcon from "@material-ui/icons/Share";
 
 import Chips from "./Chips";
 
@@ -46,8 +49,13 @@ function SingleJoke({ joke }) {
         <CardContent className={classes.chipContainer}>
           <Chips category={joke.category} flags={joke.flags} />
         </CardContent>
-        <CardActions>
-          <Button size="small">Learn More</Button>
+        <CardActions disableSpacing className={classes.CardActions}>
+          <IconButton aria-label="add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+          <IconButton aria-label="share">
+            <ShareIcon />
+          </IconButton>
         </CardActions>
       </div>
     </Card>
